1775ae3e647af67db5ee960a44f29a7249ff6cce
shoxizindayev <farhodshohizindayev@gmail.com>
2022-01-06 16:06:57 +0300

    js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+alert ("Assalomu alaykum.");
+
+var userName = prompt ("Ismingizni yozing");
+
+var userSurname = prompt ("Familiyangizni yozing");
+
+var age = prompt ("Yoshingizni yozing");
+
+var userNationality = prompt ("Millatingiz" , "O`zbek, rus, tojik va h.k");
+
+var userState = prompt ("Qayerliksiz" , "O`zbekistonlik, Tojikistonlik, Germaniyalik va h.k ");
+
+var userCity = prompt ("Yashayotgan shahringiz" , "Toshkent, Samarqand, Buxoro, New york, Varshava, Tokyo ...");
+
+var userSport = prompt ("Qaysi sport turiga qiziqasiz");
+
+var userLanguages = prompt ("Qaysi tillarni bilasiz" , "rus, arab, ingliz, turk, xitoy, koreys ...");
+
+var userEmail = prompt ("Elektron pochtangizni yozing");
+
+var userNumber = prompt ("Iltimos telefon raqamingizni yozing:");
+
+
+alert ("Rahmat.");
+
+console.log("Ismingiz: " + userName + "\n\t" + "Familiyangiz: " + userSurname + "\n" 
+    + "Yoshingiz: " + age + "\n\t"  + "Millatingiz: " + userNationality + "\n" 
+    + "Davlatingiz: " + userState + "\n\t" + "Shahringiz: " + userCity + "\n" 
+    +"Qiziqadigan sport turlaringiz: " + userSport + "\n\t" + "Biladigan tillaringiz: " + userLanguages + "\n"
+    + "Sizning Emailingiz: " + userEmail + "\n\t" + "Sizning telefon raqamingiz: " + userNumber +  "\n")
